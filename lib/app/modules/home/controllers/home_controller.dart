ed6170998655b77ddf04cb97f62794a6a05a69aa
Hamdani Ilham <d41113512@gmail.com>
2021-08-31 19:38:51 +0800

    konversi berat
    konversi berat

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
@@ -5,4 +6,54 @@ class HomeController extends GetxController {
   final provIdAsal = 0.obs;
   final hideKotaTujuan = false.obs;
   final provIdTujuan = 0.obs;
+
+  double berat = 0.0;
+  String satuan = "gram";
+  late TextEditingController beratInput;
+
+  void ubahBerat(String value) {
+    berat = double.tryParse(value) ?? 0.0;
+    String cekSatuan = satuan;
+    convertBerat(cekSatuan);
+  }
+
+  void ubahSatuan(String value) {
+    berat = double.tryParse(beratInput.text) ?? 0.0;
+    convertBerat(value);
+    satuan = value;
+  }
+
+  void convertBerat(String convert) {
+    switch (convert) {
+      case "Kwintal":
+        berat = berat * 100000;
+        break;
+      case "Kg":
+        berat = berat * 1000;
+        break;
+      case "Ons":
+        berat = berat * 100;
+        break;
+      case "Gram":
+        berat = berat;
+        break;
+      case "Mg":
+        berat = berat / 1000;
+        break;
+      default:
+        berat = berat;
+    }
+  }
+
+  @override
+  void onInit() {
+    beratInput = TextEditingController();
+    super.onInit();
+  }
+
+  @override
+  void dispose() {
+    beratInput.dispose();
+    super.dispose();
+  }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -64,7 +65,35 @@ class HomeView extends GetView<HomeController> {
                       provId: controller.provIdTujuan.value,
                       tipe: 'tujuan',
                     ))
-                : SizedBox())
+                : SizedBox()),
+            Row(
+              children: [
+                Expanded(
+                    child: TextField(
+                  autocorrect: false,
+                  controller: controller.beratInput,
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Masukkan Berat",
+                  ),
+                  onChanged: (value) => controller.ubahBerat(value),
+                )),
+                SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  width: 150,
+                  child: DropdownSearch<String>(
+                    mode: Mode.BOTTOM_SHEET,
+                    items: ["Kwintal", "Kg", "Ons", "Gram", "Mg"],
+                    label: "Satuan",
+                    selectedItem: "Gram",
+                    onChanged: (value) => controller.ubahSatuan(value!),
+                  ),
+                )
+              ],
+            )
           ],
         ));
   }
